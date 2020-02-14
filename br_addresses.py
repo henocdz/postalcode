039c94238e369d8bbc618b264175a0f66d04d9b5
henocdz <henocdz@gmail.com>
2020-02-14 09:25:18 -0600

    small improvements by persisting br timezones (dataset is incorrect)

--- br_addresses.py
+++ br_addresses.py
@@ -1,13 +1,19 @@
+import sys
 import csv
 import requests
 import json
 
 brazilian_ceps_file = "br_postalcodes.csv"
+brazil_timezones_file = "br_timezones.json"
 
 API_KEY = "RZBSUHER6MUY"
 
 
-town_tzs = {}
+with open(brazil_timezones_file, "r") as f:
+    try:
+        town_timezones = json.loads(f.read())
+    except json.decoder.JSONDecodeError:
+        town_timezones = {}
 
 with open(brazilian_ceps_file, "r") as cfile:
     csv_reader = csv.reader(cfile)
@@ -23,12 +29,12 @@ with open(brazilian_ceps_file, "r") as cfile:
         state_data["name"] = state_name
         cities = state_data.setdefault("cities", dict())
 
-        city_data = cities.setdefault(city_name, dict())
+        city_data = cities.setdefault(city_name, dict(name=city_name))
 
-        time_zone = city_data.get("time_zone", None)
-        if time_zone is None:
-            if town_name in town_tzs:
-                time_zone = town_tzs[town_name]
+        timezone = city_data.get("timezone", None)
+        if timezone is None:
+            if town_name in town_timezones:
+                timezone = town_timezones[town_name]
             else:
                 # url = f"http://vip.timezonedb.com/v2.1/get-time-zone?key={API_KEY}&format=json&by=position&lat={lat}&lng={lng}"
                 url = f"http://vip.timezonedb.com/v2.1/get-time-zone?country=BR&by=position&key={API_KEY}&format=json&fields=zoneName&lat={lat}&lng={lng}"
@@ -36,13 +42,15 @@ with open(brazilian_ceps_file, "r") as cfile:
                 response_json = response.json()
                 timezones = response_json
                 zone_name = timezones.get("zoneName")
-                time_zone = zone_name.replace("\\", "")
-                town_tzs[town_name] = time_zone
+                timezone = zone_name.replace("\\", "")
+                town_timezones[town_name] = timezone
 
-        city_data["time_zone"] = time_zone
-        city_data["name"] = city_name
+            city_data["timezone"] = timezone
 
         towns = city_data.setdefault("towns", [])
+
+        if postal_code == "01418200":
+            print(line)
 
         cep_data = dict(name=town_name, postal_code=postal_code)
         towns.append(cep_data)
@@ -51,18 +59,23 @@ with open(brazilian_ceps_file, "r") as cfile:
         per = f"{pc}%"
         if pc > last_pc:
             last_pc = pc
-            print(per)
+            sys.stdout.write(f"{per}   \r")
+            sys.stdout.flush()
 
-    addresses = []
-    for state_name, state_data in addresses_aggr.items():
-        cities_aggr = state_data.pop("cities", {})
-        cities = []
-        for city_name, city_data in cities_aggr.items():
-            cities.append(city_data)
+addresses = []
+for state_name, state_data in addresses_aggr.items():
+    cities_aggr = state_data.pop("cities", {})
+    cities = []
+    for city_name, city_data in cities_aggr.items():
+        cities.append(city_data)
 
-        state_data["cities"] = cities
+    state_data["cities"] = cities
 
-        addresses.append(state_data)
+    addresses.append(state_data)
 
-    with open("addresses_br.json", "w") as afile:
-        afile.write(json.dumps(addresses, ensure_ascii=True))
+with open("addresses_br.json", "w") as afile:
+    afile.write(json.dumps(addresses, ensure_ascii=True))
+
+
+with open(brazil_timezones_file, "w") as tzfile:
+    tzfile.write(json.dumps(town_timezones, ensure_ascii=True))
